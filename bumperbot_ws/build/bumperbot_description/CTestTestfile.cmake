be4ce7b571e066c1e8b28ce8899c0718d05cdcf1
Abood20040000 <anaalbakatoshy.gmail.com>
2025-10-05 07:03:47 +0300

    Clean repo: remove build, install, log, vscode artifacts

--- bumperbot_ws/build/bumperbot_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/abood/bumperbot_ws/src/bumperbot_description
-# Build directory: /home/abood/bumperbot_ws/build/bumperbot_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
